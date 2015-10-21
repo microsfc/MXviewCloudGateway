9641e896ef91fbb8f3012f9f9a4b6033d1bd20c3
microsfc <microsfc@gmail.com>
2015-10-21 19:30:14 +0800

    add socket.io to main dashboardI

--- client/app.js
+++ client/app.js
@@ -8,7 +8,8 @@ angular.module('mxviewCloud', [
   'ngAnimate',
   'btford.socket-io',
   'uiGmapgoogle-maps',
-  'smart-table'
+  'smart-table',
+  'btford.socket-io'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
 
@@ -58,12 +59,6 @@ angular.module('mxviewCloud', [
         }
       });
     });
-
-    $rootScope.rowCollection = [
-      {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-      {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-      {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-    ];
 
   })
 
--- client/directives/smart-table/smarttable.controller.js
+++ client/directives/smart-table/smarttable.controller.js
@@ -4,39 +4,75 @@
 'use strict';
 
 angular.module('mxviewCloud')
-  .controller('SmartTableCtrl', ['$scope', function ($scope) {
+  .controller('SmartTableCtrl', ['$scope', 'Socket', function ($scope, socketio) {
 
     //var vm = this;
 
-    var firstnames = ['Laurent', 'Blandine', 'Olivier', 'Max'];
-    var lastnames = ['Renard', 'Faivre', 'Frere', 'Eponge'];
-    var dates = ['1987-05-21', '1987-04-25', '1955-08-27', '1966-06-06'];
-    var id = 1;
-
-    function generateRandomItem(id) {
-
-      var firstname = firstnames[Math.floor(Math.random() * 3)];
-      var lastname = lastnames[Math.floor(Math.random() * 3)];
-      var birthdate = dates[Math.floor(Math.random() * 3)];
-      var balance = Math.floor(Math.random() * 2000);
-
-      return {
-        id: id,
-        firstName: firstname,
-        lastName: lastname,
-        birthDate: new Date(birthdate),
-        balance: balance
-      }
-    }
+    var MXviewSiteName = ['Site1','Site2','Site3'];
+    var informationEvent = [3,2,1];
+    var criticalEvent = [10,5,1];
+    var warningEvent = [3,5,1];
+    var id = 0;
 
     $scope.rowCollection = [];
 
-    for (id; id < 5; id++) {
-      $scope.rowCollection.push(generateRandomItem(id));
+    socketio.on('connect', function(){
+      $scope.title = 'connected';
+    });
+
+    socketio.on('user connected', function(msg){
+      $scope.promo = msg;
+    });
+
+    socketio.on('mxviewcloud dashbaord', function(msg){
+      $scope.promo = msg;
+      var mxregister_data = JSON.parse(msg);
+      for(var myKey in mxregister_data){
+        if(mxregister_data.hasOwnProperty('servername')) {
+          MXviewSiteName[0] = mxregister_data[myKey];
+
+          $scope.rowCollection[0].sitename = mxregister_data[myKey];
+
+          $scope.displayedCollection = $scope.rowCollection;
+        }
+      }
+
+      mxregister_data.forEach(function(site) {
+        console.log('test');
+      })
+    });
+
+
+    function generateItem(id) {
+      var sitename = MXviewSiteName[id];
+      var informationCount = informationEvent[id];
+      var criticalEventCount = criticalEvent[id];
+      var warningEventCount = warningEvent[id];
+
+      return {
+        id: id,
+        sitename: sitename,
+        informationEvent: informationCount,
+        criticalEvent: criticalEventCount,
+        warningEvent: warningEventCount
+      }
     }
 
-    //copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
-    $scope.displayedCollection = [].concat($scope.rowCollection);
+    function generateRandomItem(id) {
+
+      var sitename = MXviewSiteName[Math.floor(Math.random() * 3)];
+      var informationCount = informationEvent[Math.floor(Math.random() * 3)];
+      var criticalEventCount = criticalEvent[Math.floor(Math.random() * 3)];
+      var warningEventCount = warningEvent.floor(Math.random() * 2000);
+
+      return {
+        id: id,
+        sitename: sitename,
+        informationEvent: informationCount,
+        criticalEvent: criticalEventCount,
+        warningEvent: warningEventCount
+      }
+    }
 
     //add to the real data holder
     $scope.addRandomItem = function addRandomItem() {
@@ -44,13 +80,24 @@ angular.module('mxviewCloud')
       id++;
     };
 
-    //remove to the real data holder
+
+
+    for(id; id<3; id++) {
+      $scope.rowCollection.push(generateItem(id));
+    }
+
+    $scope.displayedCollection = [].concat($scope.rowCollection);
+
+    $scope.addItem = function addItem() {
+      $scope.rowCollection.push(generateItem(id));
+    }
+
     $scope.removeItem = function removeItem(row) {
       var index = $scope.rowCollection.indexOf(row);
-      if (index !== -1) {
+      if(index != -1) {
         $scope.rowCollection.splice(index, 1);
       }
-    };
+    }
 
     //angular.extend(vm, {
      // name: 'smart table'
