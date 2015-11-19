39dd096098ca9704f3e26d9a9f3bc47eef6f7891
microsfc <microsfc@gmail.com>
2015-11-19 18:57:14 +0800

    add multi sever to dash board

--- server/api/main-dashboard/main-dashboard.model.js
+++ server/api/main-dashboard/main-dashboard.model.js
@@ -3,6 +3,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose);
+
 var MainDashboardSchema = new Schema({
   regKey: String,
   deviceNormal: String,
@@ -10,5 +14,7 @@ var MainDashboardSchema = new Schema({
   deviceCritical: String
 
 });
+
+MainDashboardSchema.plugin(autoIncrement.plugin, 'MainDashboard');
 
 module.exports = mongoose.model('MainDashboard', MainDashboardSchema);
