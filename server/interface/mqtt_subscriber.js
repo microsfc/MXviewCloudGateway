cb2b8e03316725d8539ab389639dae7f13550665
microsfc <microsfc@gmail.com>
2015-11-26 17:54:40 +0800

    fixed git conflict issue

--- server/interface/mqtt_subscriber.js
+++ server/interface/mqtt_subscriber.js
@@ -7,10 +7,8 @@ module.exports = function(broker_ip, socket){
     var client  = mqtt.connect('mqtt://' + broker_ip); //192.168.127.68'
     var g_topic = "";
     var g_message = "";
-<<<<<<< HEAD
+
     var socket_io_connection_flag = 0;
-=======
->>>>>>> e32469ebeb6ad66ef93733f5424b2e0da0d66426
 
     client.on('connect', function () {
         console.log('mqtt connected');
@@ -35,13 +33,10 @@ module.exports = function(broker_ip, socket){
 
       console.log("mqtt receive = " + message.toString());
       g_message = message.toString();
-<<<<<<< HEAD
       if(socket_io_connection_flag) {
-	console.log('trigger data=',g_message);
-	socket.emit('mxviewcloud trigger_data', g_message);
+        	console.log('trigger data=',g_message);
+	        socket.emit('mxviewcloud trigger_data', g_message);
       }
-=======
->>>>>>> e32469ebeb6ad66ef93733f5424b2e0da0d66426
       //client.end();
     } );
 
@@ -50,7 +45,6 @@ module.exports = function(broker_ip, socket){
       g_topic = topic;
     };
 
-<<<<<<< HEAD
     socket.on('disconnect', function () {
       console.log('[%s] %s disconnected.', new Date().toUTCString(), socket.ip);
       socket_io_connection_flag = 0;
@@ -58,20 +52,6 @@ module.exports = function(broker_ip, socket){
 
     socket.on('connection', function(socket) {
         socket_io_connection_flag  = 1;
-	/*setInterval(function() {
-=======
-    socket.on('connection', function(socket) {
-      setInterval(function() {
->>>>>>> e32469ebeb6ad66ef93733f5424b2e0da0d66426
-        if(g_message.length > 0){
-          socket.emit('mxviewcloud trigger_data', g_message);
-          g_message = '';
-        }
-<<<<<<< HEAD
-      }, 1000);*/
-=======
-      }, 1000);
->>>>>>> e32469ebeb6ad66ef93733f5424b2e0da0d66426
     });
 
     return module;
